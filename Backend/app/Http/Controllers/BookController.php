b47609aa34fb9cb9a8ee1e0c5ac26a9adcd7f992
dohaseif2 <dohaseif2@gmail.com>
2024-07-27 23:41:33 +0300

    feat(api): update book

--- Backend/app/Http/Controllers/BookController.php
+++ Backend/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Books\StoreBookRequest;
+use App\Http\Requests\Books\UpdateBookRequest;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,30 +16,41 @@ class BookController extends Controller
         $data['user_id'] = Auth::id();
 
         $book = Book::create($data);
-        return response()->json(["book"=>$book],201);
+        return response()->json(["book" => $book], 201);
+    }
+    public function update(UpdateBookRequest $request, $id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['error' => 'Book not found']);
+        }
+        $data = $request->all();
+        $data['user_id'] = Auth::id();
+
+        $book->update($data);
+
+        return response()->json(['book' => $book], 200);
     }
     public function index()
     {
         $books = Book::all();
-        if($books->isEmpty()){
-            return response()->json(["message"=>"There is no books"],201);
+        if ($books->isEmpty()) {
+            return response()->json(["message" => "There is no books"], 201);
         }
-        return response()->json(["books"=>$books],201);
+        return response()->json(["books" => $books], 201);
     }
     public function show($slug)
     {
-        $book=Book::where('slug',$slug)->get();
-        if(!$book)
-        {
+        $book = Book::where('slug', $slug)->get();
+        if (!$book) {
             return response()->json(['error' => 'Book not found']);
         }
-        return response()->json(["book"=>$book],201);
+        return response()->json(["book" => $book], 201);
     }
     public function destroy($id)
     {
         $book = Book::find($id);
-        if(!$book)
-        {
+        if (!$book) {
             return response()->json(['error' => 'Book not found']);
         }
         $book->delete();
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -13,6 +13,6 @@ Route::prefix('books')->group(function () {
     Route::get('/', [BookController::class, 'index']);
     Route::post('/', [BookController::class, 'store'])->middleware('auth:sanctum');
     Route::get('/{slug}', [BookController::class, 'show']);
-    Route::put('/{id}', [BookController::class, 'update']);
-    Route::delete('/{id}', [BookController::class, 'destroy']);
+    Route::put('/{id}', [BookController::class, 'update'])->middleware('auth:sanctum');
+    Route::delete('/{id}', [BookController::class, 'destroy'])->middleware('auth:sanctum');
 });
