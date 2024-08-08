9a588124ae3529a1a9e0ca89b1356ca4d2e82aed
dohaseif2 <dohaseif2@gmail.com>
2024-08-08 19:55:54 +0300

    feat(api): pay

--- Backend/app/Http/Controllers/PayMobController.php
+++ Backend/app/Http/Controllers/PayMobController.php
@@ -58,4 +58,51 @@ class PayMobController extends Controller
             'url_billing' => $url_billing
         ]);
     }
+    public function callback(Request $request)
+    {
+        $data = $request->all();
+        ksort($data);
+        $hmac = $data['hmac'];
+        $array = [
+            'amount_cents',
+            'created_at',
+            'currency',
+            'error_occured',
+            'has_parent_transaction',
+            'id',
+            'integration_id',
+            'is_3d_secure',
+            'is_auth',
+            'is_capture',
+            'is_refunded',
+            'is_standalone_payment',
+            'is_voided',
+            'order',
+            'owner',
+            'pending',
+            'source_data_pan',
+            'source_data_sub_type',
+            'source_data_type',
+            'success',
+        ];
+        $connectedString = '';
+        foreach ($data as $key => $element) {
+            if (in_array($key, $array)) {
+                $connectedString .= $element;
+            }
+        }
+        $secret = env('PAYMOB_HMAC');
+        $hased = hash_hmac('sha512', $connectedString, $secret);
+        if ($hased === $hmac) {
+            $status = $data['success'];
+
+            if ($status === "true") {
+                return response()->json(['message' => 'success']);
+            } else {
+                return response()->json(['message' => 'Something Went Wrong Please Try Again']);
+            }
+        } else {
+            return response()->json(['message' => 'error']);
+        }
+    }
 }
